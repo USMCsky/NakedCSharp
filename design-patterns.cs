89848cdb8b5628d165654fb5c64c13489badc66d
Eric Zabinsky <ezabinsky@gmail.com>
2025-09-10 18:00:47 -0500

    Expanded on files. Add project.

---
+++ design-patterns.cs
@@ -0,0 +1,27 @@
+/*
+SOLID Design Principles: 
+Single Responsibility Principle
+Open-Closed Principle
+Liskov Substitution Principle
+Interface Segregation Principle
+Dependency Inversion Principle
+
+Creational Design Patterns: 
+Builder Factories (Factory Method and Abstract Factory)
+Prototype
+Singleton
+
+Structrural Design Patterns: 
+Adapter
+Bridge
+Composite
+Decorator
+Façade
+Flyweight
+Proxy
+*/
+
+//Behavioral Design Patterns:
+//Chain of Responsibility,
+//Command, Interpreter,
+//Iterator, Mediator, Memento, Null Object, Observer, State, Strategy, Template Method and Visitor
